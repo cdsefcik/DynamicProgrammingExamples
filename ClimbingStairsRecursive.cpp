e0c72f286fcfc2fb349acfff44c2c93f09f0d7e6
Christopher Sefcik <cdsefcik@gmail.com>
2026-03-15 14:02:25 -0400

    Add files via upload

---
+++ ClimbingStairsRecursive.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    
+    int countSteps(int n){
+        if(n==0 || n==1){
+            return 1;
+        }
+        return countSteps(n-1) + countSteps(n-2);
+    }
+    
+    int climbStairs(int n) {
+        return countSteps(n);
+    }
+};
